8aa4db187db8710352f3816c69c4c5e258285d27
MonarchXshubham <shubhamxmonarch@gmail.com>
2024-11-08 17:25:08 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -77,13 +77,21 @@ async def donate(bot,update):
 @Client.on_callback_query(filters.regex('home'))
 async def home_callback_handler(bot, query):
     text = f"""Hello {query.from_user.mention} \n\n➻ This Is An Advanced And Yet Powerful Rename Bot.\n\n➻ Using This Bot You Can Rename And Change Thumbnail Of Your Files.\n\n➻ You Can Also Convert Video To File Aɴᴅ File To Video.\n\n➻ This Bot Also Supports Custom Thumbnail And Custom Caption.\n\n<b>Bot Is Made By @Madflix_Bots</b>"""
-    keybord = InlineKeyboardMarkup([  
-                    [InlineKeyboardButton("📢 Updates", url="https://t.me/Madflix_Bots"),
-                    InlineKeyboardButton("💬 Support", url="https://t.me/MadflixBots_Support")],
-                    [InlineKeyboardButton("🛠️ Help", callback_data='help'),
-		            InlineKeyboardButton("❤️‍🩹 About", callback_data='about')],
-                    [InlineKeyboardButton("🧑‍💻 Developer 🧑‍💻", url="https://t.me/CallAdminRobot")]
-		  ])
+    keybord = InlineKeyboardMarkup([[
+        	    InlineKeyboardButton('🏡', callback_data='home'),
+        	    InlineKeyboardButton('⚙️', callback_data='help'),
+        	    InlineKeyboardButton('🖥', callback_data='thumbnail'),
+        	    InlineKeyboardButton('✏️', callback_data='caption'),
+	            InlineKeyboardButton('⚡️', callback_data='plan'),
+        	    InlineKeyboardButton('✨', callback_data='donate')
+        	    ],[
+	            InlineKeyboardButton('❗️ʜᴇʟᴘ', callback_data='help')
+    	            ],[
+	            InlineKeyboardButton('📜ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/vr_unreal'),
+        	    InlineKeyboardButton('📡ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/vr_support')
+        	    ],[
+  	            InlineKeyboardButton('💳ᴅᴏɴᴀᴛᴇ', callback_data='donate')
+   		    ]])
     await query.message.edit_text(text=text, reply_markup=keybord)
 
 
